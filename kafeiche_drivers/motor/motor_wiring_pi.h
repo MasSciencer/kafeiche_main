0192dab88b502171f4120131480e00ac912d70b5
sosiskovich <sosiskovich@HIHOME>
2024-09-17 20:42:57 +0800

    Implemented multitask, at this version drivers work verry well. Also pushed CMake and packege.xml

--- kafeiche_drivers/motor/motor_wiring_pi.h
+++ kafeiche_drivers/motor/motor_wiring_pi.h
@@ -1,10 +1,12 @@
-﻿#ifndef STEPPER_MOTOR_WIRING_PI_H_
+#ifndef STEPPER_MOTOR_WIRING_PI_H_
 #define STEPPER_MOTOR_WIRING_PI_H_
 
 #include <wiringPi.h>
 #include <cmath>  // Для abs()
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/float64.hpp>
+#include <thread>
+#include <atomic>
 
 constexpr uint8_t MOTOR_LEFT_DIR_PIN = 26;  // Pins
 constexpr uint8_t MOTOR_LEFT_STEP_PIN = 13;
@@ -12,82 +14,81 @@ constexpr uint8_t MOTOR_ENABLE_PIN = 12; // Sharing enable
 
 constexpr uint8_t MOTOR_RIGHT_DIR_PIN = 27;
 constexpr uint8_t MOTOR_RIGHT_STEP_PIN = 4;
-constexpr uint16_t RPI_MAX_STEPS_PER_SEC = 1000; // Количество шагов для проверки данных. Меньше шагов - чаще проверяются
 
 class StepperMotorWiringPi {
 public:
     StepperMotorWiringPi(int8_t direction_pin, int8_t step_pin, int8_t enable_pin);
-    void rotateCW(uint16_t rpm);
-    void rotateCCW(uint16_t rpm);
-    void stop();
     void setSpeed(int16_t target_rpm);
+    ~StepperMotorWiringPi();
+
 private:
+    void run();  // Функция, выполняющаяся в отдельном потоке
+    uint16_t rpmToDelay(uint16_t rpm);  // Функция для преобразования скорости в задержку
+    void stop();  // Объявление метода stop() (без тела)
+
+    std::thread motor_thread_;  // Переменная для потока
+    std::atomic<bool> running_;
+    std::atomic<int16_t> current_rpm_;
     int8_t _direction_pin;
     int8_t _step_pin;
     int8_t _enable_pin;
-    uint16_t rpmToDelay(uint16_t rpm);  // Перевод RPM в задержку между шагами
 };
 
+
 StepperMotorWiringPi::StepperMotorWiringPi(int8_t direction_pin, int8_t step_pin, int8_t enable_pin)
-    : _direction_pin(direction_pin), _step_pin(step_pin), _enable_pin(enable_pin){
-    if (wiringPiSetupGpio() < 0) {
-        throw std::runtime_error("StepperMotor wiringPi error: GPIO setup error");
+    : running_(true), current_rpm_(0), _direction_pin(direction_pin), _step_pin(step_pin), _enable_pin(enable_pin) {
+    
+        if (wiringPiSetupGpio() == -1) {  // Используем GPIO номера пинов
+        throw std::runtime_error("Failed to initialize wiringPi");
     }
+
+    // Устанавливаем режимы пинов
     pinMode(_direction_pin, OUTPUT);
     pinMode(_step_pin, OUTPUT);
     pinMode(_enable_pin, OUTPUT);
-    stop();  // Останавливаем двигатель при инициализации
+    
+    motor_thread_ = std::thread(&StepperMotorWiringPi::run, this);
 }
 
-void StepperMotorWiringPi::stop() {
-    digitalWrite(_enable_pin, HIGH);  // Отключаем мотор
-}
 
-void StepperMotorWiringPi::rotateCW(uint16_t rpm) {
-    digitalWrite(_direction_pin, LOW);
-    uint16_t step_delay = rpmToDelay(rpm);
-    digitalWrite(_enable_pin, LOW);  // Включаем мотор
-
-    // Вращаемся пока не скажут остановиться
-    for (int i = 0; i < RPI_MAX_STEPS_PER_SEC; i++) {
-        digitalWrite(_step_pin, HIGH);
-        delayMicroseconds(step_delay);
-        digitalWrite(_step_pin, LOW);
-        delayMicroseconds(step_delay);
-    }
-}
-
-void StepperMotorWiringPi::rotateCCW(uint16_t rpm) {
-    digitalWrite(_direction_pin, HIGH);
-    uint16_t step_delay = rpmToDelay(rpm);
-    digitalWrite(_enable_pin, LOW);  // Включаем мотор
-
-    for (int i = 0; i < RPI_MAX_STEPS_PER_SEC; i++) {
-        digitalWrite(_step_pin, HIGH);
-        delayMicroseconds(step_delay);
-        digitalWrite(_step_pin, LOW);
-        delayMicroseconds(step_delay);
+StepperMotorWiringPi::~StepperMotorWiringPi() {
+    running_ = false;
+    if (motor_thread_.joinable()) {
+        motor_thread_.join();
     }
 }
 
 void StepperMotorWiringPi::setSpeed(int16_t target_rpm) {
-    target_rpm = target_rpm * 100; //Возвращаем нормализованную скорость вращения с PID из диапазона -12.0 -- 12 до соответсвия реальным физическим значениям -1200 -- 1200 RPM
-    if (target_rpm > 0.2) {
-        rotateCW(target_rpm);
-    }
-    else if (target_rpm < - 0.2) {
-        rotateCCW(abs(target_rpm));
-    }
-    else {
-        stop();
+    current_rpm_ = target_rpm * 100;
+}
+
+void StepperMotorWiringPi::run() {
+    while (running_) {
+        int16_t rpm = current_rpm_;
+        if (std::abs(rpm) > 1500) {
+            uint16_t step_delay = rpmToDelay(std::abs(rpm));
+            digitalWrite(_enable_pin, LOW);
+            digitalWrite(_direction_pin, rpm > 0 ? LOW : HIGH);
+
+            digitalWrite(_step_pin, HIGH);
+            delayMicroseconds(step_delay);
+            digitalWrite(_step_pin, LOW);
+            delayMicroseconds(step_delay);
+        } else {
+            stop();
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        }
     }
 }
 
-//Основная идея регулировки с корости в ширине задержек между импульсами. Больше шаг - меньше скорость
+void StepperMotorWiringPi::stop() {
+    digitalWrite(_enable_pin, HIGH);
+}
+
 uint16_t StepperMotorWiringPi::rpmToDelay(uint16_t rpm) {
-    uint16_t steps_per_revolution = 200;  // Полный шаг: 200 шагов на оборот
+    uint16_t steps_per_revolution = 200;
     uint16_t steps_per_second = (rpm * steps_per_revolution) / 60;
-    return 1000000 / steps_per_second;  // Возвращаем задержку в микросекундах
+    return 1000000 / steps_per_second;
 }
 
 #endif  // STEPPER_MOTOR_WIRING_PI_H_
