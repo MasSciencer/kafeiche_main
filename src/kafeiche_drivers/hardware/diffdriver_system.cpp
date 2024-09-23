e2b4bd7742ffbd7afd81bcbf1ddb365c72fe5423
sosiskovich <sosiskovich@HIHOME>
2024-09-23 12:30:05 +0800

    beta 0.1 +renamed directories +!built every packagges -launch was not tasted

---
+++ src/kafeiche_drivers/hardware/diffdriver_system.cpp
@@ -0,0 +1,188 @@
+﻿#include <chrono>
+#include <cmath>
+#include <limits>
+#include <memory>
+#include <vector>
+
+#include "diffdriver_system.h"
+#include "hardware_interface/types/hardware_interface_type_values.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/float64.hpp"
+#include "control_toolbox/pid.hpp"
+
+namespace diffdriver
+{
+
+
+    hardware_interface::CallbackReturn DiffDriverKfcHardware::on_init(
+        const hardware_interface::HardwareInfo& info)
+    {
+        if (
+            hardware_interface::SystemInterface::on_init(info) !=
+            hardware_interface::CallbackReturn::SUCCESS)
+        {
+            return hardware_interface::CallbackReturn::ERROR;
+        }
+
+        // Implementation sub and creation of pub
+        auto node = rclcpp::Node::make_shared("diffdriver_node");
+
+        left_velocity_sub_ = node->create_subscription<std_msgs::msg::Float64>(
+            "/kfc/left_wheel/current_velocity", rclcpp::QoS(10),
+            [this](const std_msgs::msg::Float64::SharedPtr msg) {
+                wheel_l_.vel = msg->data;
+            });
+
+        left_position_sub_ = node->create_subscription<std_msgs::msg::Float64>(
+            "/kfc/left_wheel/position", rclcpp::QoS(10),
+            [this](const std_msgs::msg::Float64::SharedPtr msg) {
+                wheel_l_.pos = msg->data;
+            });
+        right_velocity_sub_ = node->create_subscription<std_msgs::msg::Float64>(
+            "/kfc/right_wheel/current_velocity", rclcpp::QoS(10),
+            [this](const std_msgs::msg::Float64::SharedPtr msg) {
+                wheel_r_.vel = msg->data;
+            });
+
+        right_position_sub_ = node->create_subscription<std_msgs::msg::Float64>(
+            "/kfc/right_wheel/position", rclcpp::QoS(10),
+            [this](const std_msgs::msg::Float64::SharedPtr msg) {
+                wheel_r_.pos = msg->data;
+            });
+
+        left_rpm_pub_ = node->create_publisher<std_msgs::msg::Float64>(
+            "/kfc/left_wheel/rpm", rclcpp::QoS(10));
+        right_rpm_pub_ = node->create_publisher<std_msgs::msg::Float64>(
+            "/kfc/right_wheel/rpm", rclcpp::QoS(10));
+
+
+
+        cfg_.left_wheel_name = info_.hardware_parameters["left_wheel_name"];
+        cfg_.right_wheel_name = info_.hardware_parameters["right_wheel_name"];
+
+        for (const hardware_interface::ComponentInfo& joint : info_.joints)
+        {
+            // DiffBotSystem has exactly two states and one command interface on each joint
+            if (joint.command_interfaces.size() != 1)
+            {
+                RCLCPP_FATAL(
+                    rclcpp::get_logger("DiffDriverKfcHardware"),
+                    "Joint '%s' has %zu command interfaces found. 1 expected.", joint.name.c_str(),
+                    joint.command_interfaces.size());
+                return hardware_interface::CallbackReturn::ERROR;
+            }
+
+            if (joint.command_interfaces[0].name != hardware_interface::HW_IF_VELOCITY)
+            {
+                RCLCPP_FATAL(
+                    rclcpp::get_logger("DiffDriverKfcHardware"),
+                    "Joint '%s' have %s command interfaces found. '%s' expected.", joint.name.c_str(),
+                    joint.command_interfaces[0].name.c_str(), hardware_interface::HW_IF_VELOCITY);
+                return hardware_interface::CallbackReturn::ERROR;
+            }
+
+            if (joint.state_interfaces.size() != 2)
+            {
+                RCLCPP_FATAL(
+                    rclcpp::get_logger("DiffDriverKfcHardware"),
+                    "Joint '%s' has %zu state interface. 2 expected.", joint.name.c_str(),
+                    joint.state_interfaces.size());
+                return hardware_interface::CallbackReturn::ERROR;
+            }
+
+            if (joint.state_interfaces[0].name != hardware_interface::HW_IF_POSITION)
+            {
+                RCLCPP_FATAL(
+                    rclcpp::get_logger("DiffDriverKfcHardware"),
+                    "Joint '%s' have '%s' as first state interface. '%s' expected.", joint.name.c_str(),
+                    joint.state_interfaces[0].name.c_str(), hardware_interface::HW_IF_POSITION);
+                return hardware_interface::CallbackReturn::ERROR;
+            }
+
+            if (joint.state_interfaces[1].name != hardware_interface::HW_IF_VELOCITY)
+            {
+                RCLCPP_FATAL(
+                    rclcpp::get_logger("DiffDriverKfcHardware"),
+                    "Joint '%s' have '%s' as second state interface. '%s' expected.", joint.name.c_str(),
+                    joint.state_interfaces[1].name.c_str(), hardware_interface::HW_IF_VELOCITY);
+                return hardware_interface::CallbackReturn::ERROR;
+            }
+        }
+
+        return hardware_interface::CallbackReturn::SUCCESS;
+    }
+
+    std::vector<hardware_interface::StateInterface> DiffDriverKfcHardware::export_state_interfaces()
+    {
+        std::vector<hardware_interface::StateInterface> state_interfaces;
+
+        state_interfaces.emplace_back(hardware_interface::StateInterface(
+            wheel_l_.name, hardware_interface::HW_IF_POSITION, &wheel_l_.pos));
+        state_interfaces.emplace_back(hardware_interface::StateInterface(
+            wheel_l_.name, hardware_interface::HW_IF_VELOCITY, &wheel_l_.vel));
+
+        state_interfaces.emplace_back(hardware_interface::StateInterface(
+            wheel_r_.name, hardware_interface::HW_IF_POSITION, &wheel_r_.pos));
+        state_interfaces.emplace_back(hardware_interface::StateInterface(
+            wheel_r_.name, hardware_interface::HW_IF_VELOCITY, &wheel_r_.vel));
+
+        return state_interfaces;
+    }
+
+    std::vector<hardware_interface::CommandInterface> DiffDriverKfcHardware::export_command_interfaces()
+    {
+        std::vector<hardware_interface::CommandInterface> command_interfaces;
+
+        command_interfaces.emplace_back(hardware_interface::CommandInterface(
+            wheel_l_.name, hardware_interface::HW_IF_VELOCITY, &wheel_l_.cmd));
+
+        command_interfaces.emplace_back(hardware_interface::CommandInterface(
+            wheel_r_.name, hardware_interface::HW_IF_VELOCITY, &wheel_r_.cmd));
+
+        return command_interfaces;
+    }
+
+    hardware_interface::CallbackReturn DiffDriveArduinoHardware::on_configure(
+        const rclcpp_lifecycle::State& /*previous_state*/)
+    {
+
+        pid_left_.initPid(P_p, I_p, D_p, i_max_p, i_min_p);
+        pid_right_.initPid(P_p, I_p, D_p, i_max_p, i_min_p); 
+        last_time_ = this->now();
+        last_period_ = rclcpp::Duration(0);
+
+        return hardware_interface::CallbackReturn::SUCCESS;
+    }
+
+
+    hardware_interface::return_type DiffDriverKfcHardware::read(
+        const rclcpp::Time& /*time*/, const rclcpp::Duration& period)
+    {
+        return hardware_interface::return_type::OK;
+    }
+
+    hardware_interface::return_type diffdriver::DiffDriverKfcHardware::write(
+        const rclcpp::Time& /*time*/, const rclcpp::Duration& /*period*/)
+    {
+
+        static rclcpp::Time last_time = time;
+        double dt = (time - last_time).nanoseconds() * 1e-9;  // Переводим в секунды
+        last_time = time;
+
+         // Publisher RPM
+        std_msgs::msg::Float64 left_rpm_msg;
+        left_rpm_msg.data = pid_left_.computeCommand(wheel_l_.cmd - wheel_l_.vel, dt); // Сглаживание значений через PID for left
+        left_rpm_pub_->publish(left_rpm_msg);
+
+        std_msgs::msg::Float64 right_rpm_msg;
+        right_rpm_msg.data = pid_right_.computeCommand(wheel_r_.cmd - wheel_r_.vel, dt); // Сглаживание значений через PID for right
+        right_rpm_pub_->publish(right_rpm_msg);
+
+        return hardware_interface::return_type::OK;
+    }
+
+}  // namespace diffdriver
+
+#include "pluginlib/class_list_macros.hpp"
+PLUGINLIB_EXPORT_CLASS(
+    diffdriver::DiffDriverKfcHardware, hardware_interface::SystemInterface)
---
+++ src/kafeiche_drivers/hardware/stepper_motor_wiring_pi.h
@@ -0,0 +1,109 @@
+#ifndef STEPPER_MOTOR_WIRING_PI_H_
+#define STEPPER_MOTOR_WIRING_PI_H_
+
+#include <wiringPi.h>
+#include <cmath>  // Для abs()
+#include <thread>
+#include <atomic>
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/float64.hpp"
+
+constexpr uint8_t MOTOR_LEFT_DIR_PIN = 26;  // Pins
+constexpr uint8_t MOTOR_LEFT_STEP_PIN = 13;
+constexpr uint8_t MOTOR_ENABLE_PIN = 12; // Sharing enable
+
+constexpr uint8_t MOTOR_RIGHT_DIR_PIN = 27;
+constexpr uint8_t MOTOR_RIGHT_STEP_PIN = 4;
+
+class StepperMotorWiringPi {
+public:
+    StepperMotorWiringPi(int8_t direction_pin, int8_t step_pin, int8_t enable_pin);
+    void setSpeed(int16_t target_rpm);
+    ~StepperMotorWiringPi();
+
+private:
+    void run();  // Функция, выполняющаяся в отдельном потоке
+    uint16_t rpmToDelay(uint16_t rpm);  // Функция для преобразования скорости в задержку
+    void stop();  // Объявление метода stop() (без тела)
+
+    std::thread motor_thread_;  // Переменная для потока
+    std::atomic<bool> running_;
+    std::atomic<int16_t> current_rpm_;
+    int8_t _direction_pin;
+    int8_t _step_pin;
+    int8_t _enable_pin;
+};
+
+
+StepperMotorWiringPi::StepperMotorWiringPi(int8_t direction_pin, int8_t step_pin, int8_t enable_pin)
+    : running_(true), current_rpm_(0), _direction_pin(direction_pin), _step_pin(step_pin), _enable_pin(enable_pin) {
+    
+        if (wiringPiSetupGpio() == -1) {  // Используем GPIO номера пинов
+        throw std::runtime_error("Failed to initialize wiringPi");
+    }
+
+    // Устанавливаем режимы пинов
+    pinMode(_direction_pin, OUTPUT);
+    pinMode(_step_pin, OUTPUT);
+    pinMode(_enable_pin, OUTPUT);
+    
+    motor_thread_ = std::thread(&StepperMotorWiringPi::run, this);
+}
+
+
+StepperMotorWiringPi::~StepperMotorWiringPi() {
+    running_ = false;
+    if (motor_thread_.joinable()) {
+        motor_thread_.join();
+    }
+}
+
+void StepperMotorWiringPi::setSpeed(int16_t target_rpm) {
+    current_rpm_ = target_rpm * 100;
+}
+
+void StepperMotorWiringPi::run() {
+    while (running_) {
+        int16_t rpm = current_rpm_;
+
+        //hardware evaluation of min velocity
+        if ((std::abs(rpm) > 150) && (std::abs(rpm) <= 1200)) {
+            uint16_t step_delay = rpmToDelay(std::abs(rpm));
+            digitalWrite(_enable_pin, LOW);
+            digitalWrite(_direction_pin, rpm > 0 ? LOW : HIGH);
+
+            digitalWrite(_step_pin, HIGH);
+            delayMicroseconds(step_delay);
+            digitalWrite(_step_pin, LOW);
+            delayMicroseconds(step_delay);
+        }
+
+        //hardware evaluation of max velocity
+        else if (std::abs(rpm) > 1200) {
+            uint16_t step_delay = rpmToDelay(1200);
+            digitalWrite(_enable_pin, LOW);
+            digitalWrite(_direction_pin, rpm > 0 ? LOW : HIGH);
+
+            digitalWrite(_step_pin, HIGH);
+            delayMicroseconds(step_delay);
+            digitalWrite(_step_pin, LOW);
+            delayMicroseconds(step_delay);
+
+        } else {
+            stop();
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        }
+    }
+}
+
+void StepperMotorWiringPi::stop() {
+    digitalWrite(_enable_pin, HIGH);
+}
+
+uint16_t StepperMotorWiringPi::rpmToDelay(uint16_t rpm) {
+    uint16_t steps_per_revolution = 200;
+    uint16_t steps_per_second = (rpm * steps_per_revolution) / 60;
+    return 1000000 / steps_per_second;
+}
+
+#endif  // STEPPER_MOTOR_WIRING_PI_H_
